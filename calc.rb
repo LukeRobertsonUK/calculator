24d0c40671df746407eb9a8fbf06fa9b9ddd5c95
Luke Robertson <luke.robertson@mac.com>
2013-06-18 15:41:23 +0100

    adding power function

--- calc.rb
+++ calc.rb
@@ -7,12 +7,36 @@ def menu
   gets.chomp.downcase
 end
 
+def  power
+  num_1
+  puts "... and raise it to the power:"
+  num_2
+  puts numinput1**numinput2
+end
+
+
+
+
 def basic_calc
   # ask the user which operation they want to perform
   print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
   operation = gets.chomp.downcase
 
 end
+
+def advanced_calc
+print"(p)ower, (s)quare-root:"
+operation = gets.chomp.downcase
+case operation
+when 'p'
+  power
+when 's'
+  sqrt
+else
+  puts "That is not a valid option"
+end
+
+
 
 
 # run the app...
