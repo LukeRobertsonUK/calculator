e8e3d59fce35cd28430c47974eba2ae47c108923
Luke Robertson <luke.robertson@mac.com>
2013-06-18 16:33:55 +0100

    working code

--- calc.rb
+++ calc.rb
@@ -7,27 +7,88 @@ def menu
   gets.chomp.downcase
 end
 
+def hold
+  puts "press any key to continue"
+  stop = gets.chomp
+end
+
+
 def  power
-  num_1
+  x = num_1
   puts "... and raise it to the power:"
-  num_2
-  result = numinput1**numinput2
-  puts "#{numinput1} to the power of #{numinput2} equals #{result}"
+  y = num_2
+  result = x**y
+  puts "#{x} to the power of #{y} equals #{result}"
+  hold
 end
 
-def sqrt
-  num_1
-  result = numinput1**(1/2)
-  puts "The square-root of #{numinput1} is #{result}"
+def sqrt(x)
+  result = x**(0.5)
+  puts "The square-root of #{x} is #{result}"
+  hold
 end
 
+
+def num_1
+  # ask the user for first number
+  print "First number:"
+  gets.chomp.to_f
+end
+
+def num_2
+  # ask the user for second number
+  print "Second number:"
+  gets.chomp.to_f
+end
 
 
 def basic_calc
-  # ask the user which operation they want to perform
-  print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
-  operation = gets.chomp.downcase
+ # ask the user which operation they want to perform
+ print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
+ operation = gets.chomp.downcase
+ case operation
+ when 'a'
+   add(num_1, num_2)
+ when 's'
+   sub(num_1, num_2)
+ when 'm'
+   mul(num_1, num_2)
+ when 'd'
+   div(num_1, num_2)
+ else
+ basic_calc
 end
+end
+
+#perform addition
+def add(x, y)
+ result = x + y
+ puts "#{result}"
+ hold
+end
+
+#perform subtraction
+def sub(x, y)
+ result = x - y
+ puts "#{result}"
+ hold
+end
+
+#perform multiplication
+def mul(x, y)
+ result = x * y
+ puts "#{result}"
+ hold
+end
+
+#perform division
+def div(x, y)
+ result = x / y
+ puts "#{result}"
+ hold
+end
+
+
 
 def advanced_calc
 print"(p)ower, (s)quare-root:"
@@ -36,9 +97,9 @@ case operation
 when 'p'
   power
 when 's'
-  sqrt
+  sqrt(num_1)
 else
-  puts "That is not a valid option"
+  advanced_calc
 end
 end
 
